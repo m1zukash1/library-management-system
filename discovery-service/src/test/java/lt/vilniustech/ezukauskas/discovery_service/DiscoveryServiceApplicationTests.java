95d53a4bf7b47f83c1e5ea0ad7f3cef6e043e38c
Elvinas Žukauskas <75336116+m1zukash1@users.noreply.github.com>
2025-03-30 17:09:48 +0300

    [ADD] Empty discovery-service

---
+++ discovery-service/src/test/java/lt/vilniustech/ezukauskas/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vilniustech.ezukauskas.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
